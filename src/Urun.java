61e0f5b717dca9285e40b9b49806ea7b8693e786
Harika Eda <harikaedaozdemir@gmail.com>
2023-05-16 17:35:07 +0300

    Initial commit

---
+++ src/Urun.java
@@ -0,0 +1,88 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class UrunTanimlama  {
+    public String urunIsmi;
+    public String uretici;
+    public int miktar;
+    public String birim;
+    public String raf;
+    static Map<Integer,String>urunler=new HashMap<>();
+
+    public UrunTanimlama(String urunIsmi, String uretici, int miktar, String birim, String raf) {
+        this.urunIsmi = urunIsmi;
+        this.uretici = uretici;
+        this.miktar = miktar;
+        this.birim = birim;
+        this.raf = raf;
+        this.id = id;
+    }
+
+
+    public String getUrunIsmi() {
+        return urunIsmi;
+    }
+
+    public void setUrunIsmi(String urunIsmi) {
+        this.urunIsmi = urunIsmi;
+    }
+
+    public String getUretici() {
+        return uretici;
+    }
+
+    public void setUretici(String uretici) {
+        this.uretici = uretici;
+    }
+
+    public int getMiktar() {
+        return miktar;
+    }
+
+    public void setMiktar(int miktar) {
+        this.miktar = miktar;
+    }
+
+    public String getBirim() {
+        return birim;
+    }
+
+    public void setBirim(String birim) {
+        this.birim = birim;
+    }
+
+    public String getRaf() {
+        return raf;
+    }
+
+    public void setRaf(String raf) {
+        this.raf = raf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "UrunTanimlama{" +
+                "urunIsmi='" + urunIsmi + '\'' +
+                ", uretici='" + uretici + '\'' +
+                ", miktar=" + miktar +
+                ", birim='" + birim + '\'' +
+                ", raf='" + raf + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    public int id;
+
+
+
+
+
+}
